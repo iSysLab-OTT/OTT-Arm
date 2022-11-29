d0ec5f1f197ac68d7a01e967258fee1a8b0e2530
LINKER <temp2temp@daum.net>
2022-11-30 06:17:35 +0900

    feat: select page 디자인 추가

---
+++ TCW/app/select/selectFunc.js
@@ -0,0 +1,15 @@
+// 음성 데이터 배열
+const audioCaptureArray = [];
+
+// 녹음 상태 확인 변수
+let isRecording = false;
+
+// //functions
+// function videoStart() {
+//   navigator.mediaDevices
+//     .getDisplayMedia({ video: true, audio: true })
+//     .then((stream) => {
+//       previewPlayer.srcObject = stream;
+//       startRecording(previewPlayer.captureStream());
+//     });
+// }
